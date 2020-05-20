82f8fd5c788a9114a02dad169d62c965b9121e26
TheFirm <thefirm@TheFirms-MBP.home>
2020-05-20 19:39:52 +0200

    Updates

---
+++ precache-manifest.acb0e84da5e30be48a5e423581d5db65.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd62575c27f608c9d0080896c46580e",
+    "url": "/react-testing/index.html"
+  },
+  {
+    "revision": "b29a0eb708c8ebb850d4",
+    "url": "/react-testing/static/css/main.e7c84982.chunk.css"
+  },
+  {
+    "revision": "6fcaa8cfc09f0b127624",
+    "url": "/react-testing/static/js/2.415495fd.chunk.js"
+  },
+  {
+    "revision": "9b707ee842af4f317f626a3c3900dd73",
+    "url": "/react-testing/static/js/2.415495fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29a0eb708c8ebb850d4",
+    "url": "/react-testing/static/js/main.78aa63a9.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/react-testing/static/js/main.78aa63a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8acc9b6d83e61dec57",
+    "url": "/react-testing/static/js/runtime-main.2f3a839d.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/react-testing/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/react-testing/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/react-testing/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/react-testing/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/react-testing/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/react-testing/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/react-testing/static/media/react-logo.eb6be414.png"
+  }
+]);
